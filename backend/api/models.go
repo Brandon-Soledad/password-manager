5d3d9e0ab974b6bd4e89cbfdc0e1952c36488681
Brandon Soledad <soledb25@gmail.com>
2024-06-02 18:53:38 -0700

    added Credentials, Vault, and MasterKey structs to the models file

--- backend/api/models.go
+++ backend/api/models.go
@@ -1,7 +1,25 @@
 package api
 
 type User struct {
-	ID       string `json:"id"`
+	ID       int32  `json:"id"`
 	Name     string `json:"name"`
 	Password string `json:"password"`
+}
+
+type Credentials struct {
+	ID       int32  `json:"id"`
+	UserID   int32  `json:"user_id"`
+	Service  string `json:"service"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type Vault struct {
+	UserID      int32
+	Credentials []Credentials
+}
+
+type MasterKey struct {
+	UserID int32
+	Key    []byte
 }
