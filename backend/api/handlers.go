437cd7ed8eacc2815824e4e690221a2c8a809987
Brandon Soledad <soledb25@gmail.com>
2024-06-06 21:49:37 -0700

    Added post request handler for creating a user

--- backend/api/handlers.go
+++ backend/api/handlers.go
@@ -1,1 +1,14 @@
 package api
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func createUser(c *gin.Context) {
+	var user User
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+}
